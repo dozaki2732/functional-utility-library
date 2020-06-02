630e7af6727ac52a641db1ac899ca03498fc6809
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-02 14:08:52 -0700

    created filter function

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-// Your functions go here!
+var numberList = [2, 4, 7, 23, 64, 534, 7, 45, 265];
+
+Array.prototype.myFilter = function (callback) {
+   var newArray = [];
+   for (let i = 0; i < numberList.length; i++) {
+      if (callback(numberList[i]) === true) {
+         newArray.push(numberList[i]);
+      }
+   }
+   console.log(newArray);
+   return newArray;
+};
+function implementFilter(input) {
+   var new_list = numberList.myFilter(function (item) {
+      //console.log(new_list);
+      return item % input === 0;
+   });
+   return new_list;
+}
