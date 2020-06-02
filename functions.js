395680e08de68f50bb3705dd2c55e1f0029ff26a
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-02 16:20:38 -0700

    adding slice functions

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 var numberList = [2, 4, 7, 23, 64, 534, 7, 45, 265];
-
+//filter the array of numbers by seeing what input it is divisible by
 Array.prototype.myFilter = function (callback) {
    var newArray = [];
    for (let i = 0; i < numberList.length; i++) {
@@ -10,10 +10,36 @@ Array.prototype.myFilter = function (callback) {
    console.log(newArray);
    return newArray;
 };
+// see if the number is divisible by the input number
 function implementFilter(input) {
    var new_list = numberList.myFilter(function (item) {
-      //console.log(new_list);
+      console.log(new_list);
+      //see if the remainder for the number divided by input is 0 (it can be divided by that number)
       return item % input === 0;
    });
    return new_list;
+}
+
+//return slice of an array
+var listOfAnimals = [
+   " goat",
+   " dog",
+   " bird",
+   " rat",
+   " penguin",
+   " tiger",
+   " elephant",
+];
+
+function sliceArray(begin, end) {
+   let slicedArray = listOfAnimals.slice(begin, end);
+   return slicedArray;
+}
+
+//use slice instead of splice to remove elements from an array
+var listofBeer = ["kirin", "corona", "modelo", "sapporo", "pabst", "stella"];
+
+function removeWithSlice(begin, end) {
+   let reducedArray = listofBeer.slice(begin, end);
+   return reducedArray;
 }
